9ca58e4e4bf72cdc4f1c58099ed6f1f868d4e287
Hayat ilyas <ihayat855@gmail.com>
2025-05-25 15:46:03 +0530

    Setup database and API server

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,3 +1,8 @@
 import logger from "@utils/logger";
+import startServer from "./server";
 
-logger.info("Hello Start reading file");
+const server = startServer();
+
+server.listen(9000, () => {
+  logger.info("Server is running on port 9000");
+});
--- apps/server/src/server.ts
+++ apps/server/src/server.ts
@@ -1,8 +1,26 @@
-import fs from "fs/promises";
+import { httpStatus } from "@customtype/http";
+import ErrorMiddleware from "@middlewares/error-middleware";
+import express, { type Express, Request, Response } from "express";
+import morgan from "morgan";
 
-const startReading = async (fileName: string) => {
-  const data = await fs.readFile(__dirname + fileName, "utf8");
-  return data;
+const startServer = (): Express => {
+  const app = express();
+
+  app.use(morgan("tiny"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  app.disable("x-powered-by");
+
+  app.get("/", (req: Request, res: Response) => {
+    res.status(200).json({
+      status: httpStatus.SUCCESS,
+      message: "Pragya Ecom is running",
+    });
+  });
+
+  app.use(ErrorMiddleware);
+
+  return app;
 };
 
-export { startReading };
+export default startServer;
